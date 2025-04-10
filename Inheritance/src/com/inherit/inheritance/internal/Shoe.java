a3684affc61d629c1bbc1cef6b7cc9d4c93a97bd
Pruthvikanth L <157116170+PruthvikanthL@users.noreply.github.com>
2025-04-11 02:01:12 +0530

    Delete Inheritance/src/com/inherit/inheritance directory

--- Inheritance/src/com/inherit/inheritance/internal/Shoe.java
+++
@@ -1,7 +0,0 @@
-package com.inherit.inheritance.internal;
-
-public class Shoe extends Weapon {
-    protected Shoe(){
-        System.out.println("no-arg Shoe const is running ");
-    }
-}
--- Inheritance/src/com/inherit/inheritance/internal/Weapon.java
+++
@@ -1,9 +0,0 @@
-package com.inherit.inheritance.internal;
-
-public class Weapon {
-    public Weapon(){
-        System.out.println("no-arg weapon parent class is running");
-
-    }
-
-}
